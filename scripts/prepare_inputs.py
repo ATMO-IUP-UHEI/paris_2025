b03c0f7d81f4361e9981dbd01830d18990a3f2bb
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-09-18 11:45:18 -0400

    Add input preparation functions for domain, terrain, landcover, buildings, and fluxes

---
+++ scripts/prepare_inputs.py
@@ -0,0 +1,63 @@
+import logging
+from pathlib import Path
+
+import ggpymanager as ggp
+import xarray as xr
+
+import paris_2025 as p
+from paris_2025 import CONFIG
+
+
+def prepare_domain(
+    # crs, gral_bbox, gral_dx, gral_dy, gramm_bbox, gramm_dx, gramm_dy, output_path
+):
+    logging.info("Preparing domain...")
+
+
+def prepare_terrain():
+    logging.info("Preparing terrain...")
+    gramm_conf = CONFIG["domain"]["gramm"]
+    geometry_file = Path(gramm_conf["conf_path"]) / "ggeom.asc"
+    if geometry_file.exists():
+        logging.info(f"Geometry file {geometry_file} already exists. Skipping terrain.")
+        return
+    gramm_terrain_path = p.model_input.terrain.process_terrain(only_check_status=False)
+    elevation = xr.open_dataset(gramm_terrain_path)["elevation"]
+    elevation = ggp.gramm_geometry.smooth_elevation(elevation)
+    geom = ggp.gramm_geometry.create_ggeom_dataset(
+        elevation=elevation,
+        nz=gramm_conf["nz"],
+        z0=gramm_conf["z0"],
+        vert_stretching=gramm_conf["vert_stretching"],
+    )
+    ggp.gramm_geometry.write_ggeom_file(geom, file_path=geometry_file)
+
+
+def prepare_landcover():
+    logging.info("Preparing landcover...")
+    landuse_file = Path(CONFIG["domain"]["gramm"]["conf_path"]) / "landuse.asc"
+    if landuse_file.exists():
+        logging.info(f"Landuse file {landuse_file} already exists. Skipping landcover.")
+        return
+    gramm_landcover_path = p.model_input.landcover.process_landcover()
+    landcover = xr.open_dataset(gramm_landcover_path)
+    ggp.utils.write_landuse(landuse_file, landcover)
+
+
+def prepare_buildings():
+    logging.info("Preparing buildings...")
+
+
+def prepare_fluxes():
+    logging.info("Preparing fluxes...")
+
+
+if __name__ == "__main__":
+    # Get the root logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    prepare_domain()
+    prepare_terrain()
+    prepare_landcover()
+    prepare_fluxes()
