334572e900be96037047edfb390cb97790a3c0cb
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-09-19 15:19:55 -0400

    Refactor fluxes module: update figure paths, add VPRM unit conversion, and enhance area flux processing

--- src/paris_2025/model_input/fluxes.py
+++ src/paris_2025/model_input/fluxes.py
@@ -6,11 +6,24 @@ import ggpymanager as ggp
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
+from rasterio.enums import Resampling
 
 from paris_2025 import CONFIG
 
 AREA_ID_NETCDF_PATH = Path(CONFIG["data_path"]) / "Fluxes/area_id.nc"
-FIGURE_PATH = Path(CONFIG["figures_path"]) / "Input/area_id_overview.png"
+FIGURE_PATHS = {
+    "area_id": Path(CONFIG["figures_path"]) / "Input/area_id_overview.png",
+    "area_fluxes": Path(CONFIG["figures_path"]) / "Input/area_fluxes.png",
+}
+
+VPRM_2023_R_NETCDF_PATH = Path(CONFIG["data_path"]) / "Fluxes/area_flux_vprm_2023_R.nc"
+VPRM_2023_GEE_NETCDF_PATH = (
+    Path(CONFIG["data_path"]) / "Fluxes/area_flux_vprm_2023_GEE.nc"
+)
+VPRM_2024_R_NETCDF_PATH = Path(CONFIG["data_path"]) / "Fluxes/area_flux_vprm_2024_R.nc"
+VPRM_2024_GEE_NETCDF_PATH = (
+    Path(CONFIG["data_path"]) / "Fluxes/area_flux_vprm_2024_GEE.nc"
+)
 
 
 def create_area_partitioning():
@@ -81,7 +94,7 @@ def create_area_partitioning():
         logging.info(f"File {AREA_ID_NETCDF_PATH} exists, not overwriting.")
     gral_grid = xr.open_dataset(AREA_ID_NETCDF_PATH)
 
-    if not FIGURE_PATH.exists():
+    if not FIGURE_PATHS["area_id"].exists():
         minx, maxx = gral_grid.x.values[[0, -1]]
         miny, maxy = gral_grid.y.values[[0, -1]]
         plt.hlines(gral_grid["ybins"], minx, maxx, label="Bins")
@@ -91,4 +104,71 @@ def create_area_partitioning():
         plt.xlabel("X (m)")
         plt.ylabel("Y (m)")
         plt.legend(loc="upper right")
-        plt.savefig(FIGURE_PATH)
+        plt.savefig(FIGURE_PATHS)
+
+
+def convert_vprm_units(vprm):
+    """Convert VPRM units from umol CO2 / m2 / s to kg CO2 / m2 / h"""
+    molar_mass_co2 = 44.01  # g/mol
+    conversion_factor = molar_mass_co2 / 1e6 * 3600 / 1000  # kg / umol * s to h
+
+    for var in vprm.data_vars:
+        attrs = vprm[var].attrs
+        assert attrs["units"] == "mu mol CO2 m-2 s-1"
+        attrs["units"] = "kg CO2 m-2 h-1"
+        attrs["long_name"] = "Annual mean CO2 flux"
+        vprm[var] = vprm[var] * conversion_factor
+        vprm[var].attrs = attrs
+    return vprm
+
+
+def create_vprm_area_fluxes():
+    area_id = xr.open_dataset(AREA_ID_NETCDF_PATH)
+    vprm_crs = "EPSG:32631"
+    vprm = {}
+    for year in [2023, 2024]:
+        vprm[year] = xr.open_dataset(
+            f"/Users/rmaiwald/Levante/Paris/Input/Fluxes/VPRM/vprm_mean_{year}.nc"
+        ).rio.write_crs(vprm_crs)
+        vprm[year] = vprm[year].rio.reproject_match(
+            area_id, resampling=Resampling.average
+        )
+        vprm[year] = vprm[year].fillna(0)
+        vprm[year] = convert_vprm_units(vprm[year])
+
+    for year, r_path, gee_path in [
+        (2023, VPRM_2023_R_NETCDF_PATH, VPRM_2023_GEE_NETCDF_PATH),
+        (2024, VPRM_2024_R_NETCDF_PATH, VPRM_2024_GEE_NETCDF_PATH),
+    ]:
+        for flux_type, path in [("R", r_path), ("GEE", gee_path)]:
+            if not path.exists():
+                logging.info(f"Writing {path}")
+                ds = (
+                    vprm[year][flux_type]
+                    .to_dataset(name="flux")
+                    .expand_dims(dim={"type": [f"VPRM {year} {flux_type}"]}, axis=0)
+                )
+                ds["type"].attrs["long_name"] = "Flux type"
+                timestamp = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+                ds.attrs["description"] = f"Dataset compiled {timestamp} EDT"
+                ds.to_netcdf(path)
+            else:
+                logging.info(f"{path} exists, skipping.")
+
+
+def plot_area_fluxes():
+    if not FIGURE_PATHS["area_fluxes"].exists():
+        logging.info(f"Plotting area fluxes to {FIGURE_PATHS['area_fluxes']}")
+        xr.open_mfdataset(
+            str(Path(CONFIG["data_path"]) / "Fluxes/area_flux_*.nc")
+        ).flux.plot(col="type", col_wrap=4)
+        plt.savefig(FIGURE_PATHS["area_fluxes"])
+    else:
+        logging.info(f"File {FIGURE_PATHS['area_fluxes']} exists, skipping plot.")
+
+
+def process_fluxes():
+    create_area_partitioning()
+    
+    create_vprm_area_fluxes()
+    plot_area_fluxes()
