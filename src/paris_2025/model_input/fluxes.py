e6a4f1320fbd780fcee8a1b45db8653be5699e42
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-09-19 14:11:55 -0400

    Refactor area partitioning logic in create_area_partitioning function; add xbins and ybins attributes to gral_grid

--- src/paris_2025/model_input/fluxes.py
+++ src/paris_2025/model_input/fluxes.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from pathlib import Path
 
 import ggpymanager as ggp
@@ -17,34 +18,74 @@ def create_area_partitioning():
     Create a partitioning of the model domain into areas and save it as a NetCDF file.
     """
 
-    nx = CONFIG["fluxes"]["nx_areas"]
-    ny = CONFIG["fluxes"]["ny_areas"]
-
-    gral_grid = ggp.utils.create_domain_grid("gral", CONFIG)
-    minx, maxx = gral_grid.x.values[[0, -1]]
-    miny, maxy = gral_grid.y.values[[0, -1]]
-    xbins = np.linspace(minx, maxx, nx + 1)
-    ybins = np.linspace(miny, maxy, ny + 1)
-    gral_grid["area_id"] = (
-        ("y", "x"),
-        np.nan * np.ones(gral_grid.y.shape + gral_grid.x.shape),
-    )
-    for i in range(nx):
-        for j in range(ny):
-            gral_grid["area_id"].loc[
-                dict(x=slice(xbins[i], xbins[i + 1]), y=slice(ybins[j], ybins[j + 1]))
-            ] = (i + j * nx)
-
     if not AREA_ID_NETCDF_PATH.exists():
+        nx = CONFIG["fluxes"]["nx_areas"]
+        ny = CONFIG["fluxes"]["ny_areas"]
+
+        gral_grid = ggp.utils.create_domain_grid("gral", CONFIG)
+        minx, maxx = gral_grid.x.values[[0, -1]]
+        miny, maxy = gral_grid.y.values[[0, -1]]
+        xbins = np.linspace(minx, maxx, nx + 1)
+        ybins = np.linspace(miny, maxy, ny + 1)
+        gral_grid["xbins"] = (("xbins"), xbins)
+        gral_grid["ybins"] = (("ybins"), ybins)
+        gral_grid = gral_grid.assign_coords(xbins=xbins, ybins=ybins)
+
+        gral_grid["area_id"] = (
+            ("y", "x"),
+            np.nan * np.ones(gral_grid.y.shape + gral_grid.x.shape),
+        )
+        for i in range(nx):
+            for j in range(ny):
+                gral_grid["area_id"].loc[
+                    dict(
+                        x=slice(xbins[i], xbins[i + 1]), y=slice(ybins[j], ybins[j + 1])
+                    )
+                ] = (i + j * nx)
+
+        # Add attrs
+        timestamp = datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+        gral_grid.attrs["description"] = f"Dataset compiled {timestamp} EDT"
+        gral_grid["area_id"].attrs = {
+            "long_name": "Area ID",
+            "description": f"Partitioning of the model domain into {nx} x {ny} areas",
+            "units": "1",
+            "standard_name": "area_id",
+        }
+        gral_grid["xbins"].attrs = {
+            "long_name": "X bin edges",
+            "units": "m",
+            "standard_name": "projection_x_coordinate",
+        }
+        gral_grid["ybins"].attrs = {
+            "long_name": "Y bin edges",
+            "units": "m",
+            "standard_name": "projection_y_coordinate",
+        }
+        gral_grid["x"].attrs = {
+            "long_name": "Easting",
+            "units": "m",
+            "standard_name": "projection_x_coordinate",
+        }
+        gral_grid["y"].attrs = {
+            "long_name": "Northing",
+            "units": "m",
+            "standard_name": "projection_y_coordinate",
+        }
+
         logging.info(f"Writing area_id to {AREA_ID_NETCDF_PATH}")
-        gral_grid[["x", "y", "area_id"]].to_netcdf(AREA_ID_NETCDF_PATH)
+        gral_grid[["x", "y", "xbins", "ybins", "area_id"]].to_netcdf(
+            AREA_ID_NETCDF_PATH
+        )
     else:
         logging.info(f"File {AREA_ID_NETCDF_PATH} exists, not overwriting.")
     gral_grid = xr.open_dataset(AREA_ID_NETCDF_PATH)
 
     if not FIGURE_PATH.exists():
-        plt.hlines(ybins, minx, maxx, label="Bins")
-        plt.vlines(xbins, miny, maxy)
+        minx, maxx = gral_grid.x.values[[0, -1]]
+        miny, maxy = gral_grid.y.values[[0, -1]]
+        plt.hlines(gral_grid["ybins"], minx, maxx, label="Bins")
+        plt.vlines(gral_grid["xbins"], miny, maxy)
         gral_grid.area_id.plot()
         plt.title("Area ID")
         plt.xlabel("X (m)")
