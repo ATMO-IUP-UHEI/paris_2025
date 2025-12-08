797e7b363955eece16c2503395f7038acd3d11ad
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-12-08 09:30:31 +0100

    Refactor create_figures_if_missing function to streamline figure creation checks and logging

--- scripts/create_figures.py
+++ scripts/create_figures.py
@@ -1,10 +1,14 @@
+import logging
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 
-
 import paris_2025 as p
 import paris_2025.plotting as plotting
+
+# Get the root logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 YEAR = 2023
 FIGURE_PATH = Path(p.CONFIG["figures_path"])
@@ -30,54 +34,81 @@ plt.rcParams.update(
         "savefig.dpi": 300,
     }
 )
+
+
+def create_figures_if_missing(fig_paths, plot_function):
+    """Create figures only if any of them don't exist."""
+    if isinstance(fig_paths, (list, tuple)):
+        if any(not path.exists() for path in fig_paths):
+            logging.info(f"Creating figures:\n{[str(p) for p in fig_paths]}")
+            plot_function(*fig_paths)
+        else:
+            logging.info(
+                "Figures already exist:\n"
+                + ("\n".join([str(p) for p in fig_paths if p.exists()]))
+            )
+    else:
+        if not fig_paths.exists():
+            logging.info(f"Creating figure:\n{fig_paths}")
+            plot_function(fig_paths)
+        else:
+            logging.info(f"Figure already exists:\n{fig_paths}")
+
+
 if __name__ == "__main__":
+    # Concentration from catalog
     DIR = "concentration_from_catalog"
     (FIGURE_PATH / DIR).mkdir(parents=True, exist_ok=True)
-    fig_path_1 = FIGURE_PATH / DIR / "source_group_contribution_stations.png"
-    fig_path_2 = FIGURE_PATH / DIR / "source_group_area_contribution_stations.png"
-    if not fig_path_1.exists() or not fig_path_2.exists():
-        (plotting.tracer_from_catalog.plot_source_group_contribution_to_stations)(
-            fig_path_1, fig_path_2
-        )
+    create_figures_if_missing(
+        [
+            FIGURE_PATH / DIR / "source_group_contribution_stations.png",
+            FIGURE_PATH / DIR / "source_group_area_contribution_stations.png",
+        ],
+        plotting.tracer_from_catalog.plot_source_group_contribution_to_stations,
+    )
 
+    # Wind measurements
     DIR = "wind_measurements"
-    fig_path = FIGURE_PATH / DIR / "wind_roses_measurements.png"
-    if not fig_path.exists():
-        p.plotting.meteo_measurements.plot_wind_roses_of_meteo_measurements(fig_path)
+    (FIGURE_PATH / DIR).mkdir(parents=True, exist_ok=True)
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "wind_roses_measurements.png",
+        p.plotting.meteo_measurements.plot_wind_roses_of_meteo_measurements,
+    )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "meteo_overview.png",
+        p.plotting.meteo_measurements.plot_meteo_overview,
+    )
+    create_figures_if_missing(
+        [
+            FIGURE_PATH / DIR / "wind_data_availability_2023.png",
+            FIGURE_PATH / DIR / "wind_data_availability_2024.png",
+        ],
+        p.plotting.meteo_measurements.plot_wind_data_availability,
+    )
 
-    fig_path = FIGURE_PATH / DIR / "meteo_overview.png"
-    if not fig_path.exists():
-        p.plotting.meteo_measurements.plot_meteo_overview(fig_path)
-
-    fig_path_1 = FIGURE_PATH / DIR / "wind_data_availability_2023.png"
-    fig_path_2 = FIGURE_PATH / DIR / "wind_data_availability_2024.png"
-    if not fig_path_1.exists() or not fig_path_2.exists():
-        p.plotting.meteo_measurements.plot_wind_data_availability(
-            fig_path_1, fig_path_2
-        )
-
+    # CO2 measurements
     DIR = "co2_measurements"
     (FIGURE_PATH / DIR).mkdir(parents=True, exist_ok=True)
-
-    fig_path = FIGURE_PATH / DIR / "co2_concentration_heatmap.png"
-    if not fig_path.exists():
-        p.plotting.tracer_measurements.plot_co2_concentration_heatmap(fig_path)
-
-    fig_path = FIGURE_PATH / DIR / "co2_by_height_and_instrument.png"
-    if not fig_path.exists():
-        p.plotting.tracer_measurements.plot_co2_by_height_and_instrument(fig_path)
-
-    fig_path = FIGURE_PATH / DIR / "average_co2_spatial.png"
-    if not fig_path.exists():
-        p.plotting.tracer_measurements.plot_average_co2_spatial(fig_path)
-
-    fig_path = FIGURE_PATH / DIR / "co2_instruments_map.png"
-    if not fig_path.exists():
-        p.plotting.tracer_measurements.plot_co2_instruments_map(fig_path)
-
-    fig_path_1 = FIGURE_PATH / DIR / "co2_data_availability_2023.png"
-    fig_path_2 = FIGURE_PATH / DIR / "co2_data_availability_2024.png"
-    if not fig_path_1.exists() or not fig_path_2.exists():
-        p.plotting.tracer_measurements.plot_co2_data_availability(
-            fig_path_1, fig_path_2
-        )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "co2_concentration_heatmap.png",
+        p.plotting.tracer_measurements.plot_co2_concentration_heatmap,
+    )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "co2_by_height_and_instrument.png",
+        p.plotting.tracer_measurements.plot_co2_by_height_and_instrument,
+    )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "average_co2_spatial.png",
+        p.plotting.tracer_measurements.plot_average_co2_spatial,
+    )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "co2_instruments_map.png",
+        p.plotting.tracer_measurements.plot_co2_instruments_map,
+    )
+    create_figures_if_missing(
+        [
+            FIGURE_PATH / DIR / "co2_data_availability_2023.png",
+            FIGURE_PATH / DIR / "co2_data_availability_2024.png",
+        ],
+        p.plotting.tracer_measurements.plot_co2_data_availability,
+    )
