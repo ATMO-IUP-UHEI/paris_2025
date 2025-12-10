1b28b1c02a41f933c116e501d2783ecd061bf4a3
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2025-12-10 11:38:11 +0100

    Add function to plot wind components by stability class and update figure creation

--- scripts/create_figures.py
+++ scripts/create_figures.py
@@ -93,6 +93,10 @@ if __name__ == "__main__":
         FIGURE_PATH / DIR / "wind_speed_by_altitude_non_lidar_2023.png",
         p.plotting.meteo_measurements.plot_wind_speed_by_altitude_non_lidar,
     )
+    create_figures_if_missing(
+        FIGURE_PATH / DIR / "wind_components_by_stability_class_2023.png",
+        p.plotting.meteo_measurements.plot_gral_wind_components_by_stability_class,
+    )
 
     # CO2 measurements
     DIR = "co2_measurements"
