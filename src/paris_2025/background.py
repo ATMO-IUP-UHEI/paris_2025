0d234494f3ba4fc4e515a1df4b0eb946a903c131
Robert Maiwald <robert.maiwald@uni-heidelberg.de>
2026-01-12 16:19:23 +0100

    Refactor get_background_co2 function to remove year parameter and improve logging for CO2 measurements

--- src/paris_2025/background.py
+++ src/paris_2025/background.py
@@ -1,4 +1,6 @@
 import numpy as np
+import xarray as xr
+import logging
 
 import paris_2025 as p
 
@@ -24,35 +26,30 @@ def get_wind_direction():
     return normalized_mean_u_wind, normalized_mean_v_wind
 
 
-def get_background_co2(year: str):
+def get_background_co2() -> xr.Dataset:
     """
-    Get the dynamic background CO2 measurements for a given year.
-
-    Parameters
-    ----------
-    year : str
-        The year for which to get the background CO2 measurements, e.g., '2023'.
+    Get the background CO2 measurements.
 
     Returns
     -------
     dynamic_background : xarray.Dataset
-        The dynamic background CO2 measurements for the specified year.
+        The dynamic background CO2 measurements.
     """
-    if not isinstance(year, str):
-        raise TypeError("Year must be a string representing the year, e.g., '2023'.")
-
     # Get mean wind direction
+    logging.info("Getting wind direction for background CO2 computation...")
     normalized_mean_u_wind, normalized_mean_v_wind = get_wind_direction()
-    normalized_mean_u_wind = normalized_mean_u_wind.sel(time=year)
-    normalized_mean_v_wind = normalized_mean_v_wind.sel(time=year)
+    normalized_mean_u_wind = normalized_mean_u_wind
+    normalized_mean_v_wind = normalized_mean_v_wind
 
     # Get GRAL domain centroid
+    logging.info("Getting GRAL domain centroid...")
     centroid_x, centroid_y = p.domain.get_centroid_of_domain("gral")
 
-    # Get background CO2 measurements
-    co2 = p.tracers.get_co2_measurements()
-    co2 = co2.sel(time=year)
+    # Get CO2 measurements
+    logging.info("Getting background CO2 measurements...")
+    co2 = p.tracers.get_co2_measurements().load()
 
+    logging.info("Filtering background CO2 stations...")
     is_high_cost = co2.instrument == "Picarro"
     background_co2 = co2.sel(station=is_high_cost & ~co2.in_gral_domain)
     # Filter out OVS_20 and GNS_36 due to local contamination
@@ -62,6 +59,7 @@ def get_background_co2(year: str):
         station=~background_co2.station.isin(["OVS_20", "GNS_36"])
     )
 
+    logging.info("Computing normalized distances of background CO2 stations...")
     x = background_co2.x - centroid_x
     y = background_co2.y - centroid_y
     length = np.sqrt(x**2 + y**2)
@@ -84,8 +82,30 @@ def get_background_co2(year: str):
     )
 
     # Set distances to NaN if no co2 measurements are available
+    logging.info("Selecting dynamic background CO2 stations...")
     distances = distances.where(background_co2["co2"].notnull())
 
     dynamic_background_index = distances.idxmin(dim="station")
-    dynamic_background = background_co2.sel(station=dynamic_background_index)
+    hours_without_background = dynamic_background_index.isnull().sum().item()
+    logging.info(
+        f"Hours without dynamic background station: {hours_without_background}"
+    )
+    logging.info(
+        f"Unique dynamic background stations: "
+        f"{dynamic_background_index.to_pandas().unique()}"
+    )
+
+    # Create placeholder for nan-values
+    placeholder = xr.full_like(background_co2.isel(station=0), fill_value=np.nan)
+    for coord in placeholder.coords:
+        if coord == "time":
+            continue
+        else:
+            placeholder[coord] = np.full(placeholder[coord].shape, np.nan)
+
+    selection = xr.concat(
+        [background_co2, placeholder],
+        dim="station",
+    )
+    dynamic_background = selection.sel(station=dynamic_background_index)
     return dynamic_background
